d5e69c53ba5d5fa0a6fcf66ad4a7a45f6d876194
jonahjohnzon <jonahjohnzon@gmail.com>
2025-01-29 18:09:04 +0100

    push

--- app/api/getmovie/route.js
+++ app/api/getmovie/route.js
@@ -67,9 +67,7 @@ export const GET = async (req) => {
     });
 
     const page = await browser.newPage();
-    await page.setUserAgent(
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    );
+ 
     await page.goto(pageUrl, { waitUntil: "domcontentloaded" });
     const m3u8Url = await new Promise((resolve, reject) => {
       const timeout = setTimeout(() => {
